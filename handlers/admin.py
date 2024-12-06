1c4cd47ded5978e133ddbd2136f3d2b80e2f02e4
hosternus <90009071+hosternus@users.noreply.github.com>
2024-12-06 23:17:58 +0300

    num of users for admin

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,3 +1,13 @@
 from aiogram import Router
 
+from aiogram.filters.command import Command
+from aiogram.types import Message
+
+from middleware import getNumberOfUsers
+from locales.localization import AdminRuLocal
+
 router = Router()
+
+@router.message(Command('admin'))
+async def numberOfUsers(msg: Message):
+    await msg.reply(AdminRuLocal.numOfUsers + str(getNumberOfUsers()))
